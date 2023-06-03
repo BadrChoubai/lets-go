184cf4e01499b267733c0a83617fdef98e85a847
Badr <choubai.badr@protonmail.com>
2023-06-03 16:16:27 -0600

    3.1 - Managing Configuration Settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "Http Network Address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	// Create file server to serve files out of "./ui/static"
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-
 	// Create handler from the file server that serves all requests to /static/,
 	// stripping "/static" before a request reaches the file server
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -19,7 +23,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on http://127.0.0.1:4000")
-	err := http.ListenAndServe("127.0.0.1:4000", mux)
+	log.Printf("Starting server on http://127.0.0.1%s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
