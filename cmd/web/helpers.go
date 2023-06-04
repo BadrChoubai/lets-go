cb3eb474895310455e608c4fb93f9a21d19b1513
Badr <choubai.badr@protonmail.com>
2023-06-03 21:02:09 -0600

    Refactor helper implementation to log errors at proper level

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,7 +11,7 @@ import (
 // debug.Stack https://pkg.go.dev/runtime/debug#Stack
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Print(trace)
+	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
