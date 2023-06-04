2c5b77a03fe0eaaae606ec11ecc5ff0f014d8cff
Badr <choubai.badr@protonmail.com>
2023-06-03 21:54:08 -0600

    Start to implement database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Initialize a new instance of our application struct, containing dependencies.
@@ -15,6 +18,8 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "Http Network Address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySql Data Source Name")
+
 	flag.Parse()
 
 	// A big benefit of logging your messages to the standard streams (stdout and stderr) like we are is that your
@@ -23,6 +28,13 @@ func main() {
 	// go run ./cmd/web >>/tmp/info.log 2>>/tmp/error.log
 	infoLog := log.New(os.Stdin, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDb(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -36,6 +48,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on http://127.0.0.1%s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
