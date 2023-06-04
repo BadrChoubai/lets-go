a6fa5e2ac3c18ffe982cfdd9d7bc0a61555c339e
Badr <choubai.badr@protonmail.com>
2023-06-04 14:12:05 -0600

    Add code for template cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,45 @@
 package main
 
-import "snippetbox.badrchoubai.dev/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+	"snippetbox.badrchoubai.dev/internal/models"
+)
 
 type TemplateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// Initialize a new map to act as the cache.
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob() function to get a slice of all filepaths that
+	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
+	// us a slice of all the filepaths for our application 'page' templates
+	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
