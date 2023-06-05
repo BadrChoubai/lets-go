076d3f76d8a86c43dc058baee4e52552a963ce0c
Badr <choubai.badr@protonmail.com>
2023-06-04 19:44:27 -0600

    grammar

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -7,8 +7,9 @@ import (
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -16,7 +17,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
 	// Use the filepath.Glob() function to get a slice of all filepaths that
-	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
+	// match the pattern "./ui/html/pages/*.tmpl". This will essentially give
 	// us a slice of all the filepaths for our application 'page' templates
 	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
