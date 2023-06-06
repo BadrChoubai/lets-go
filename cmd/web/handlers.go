958fa42735c7621a3ea99a13837bced1027dbf6b
Badr <choubai.badr@protonmail.com>
2023-06-05 19:37:28 -0600

    Create data type around form input

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -56,6 +56,13 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
+type snippetCreateForm struct {
+	Title       string
+	Expires     int
+	FieldErrors map[string]string
+	Content     string
+}
+
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -63,37 +70,43 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+
 	fieldErrors := make(map[string]string)
 
-	if strings.TrimSpace(title) == "" {
-		fieldErrors["title"] = "field: title, may not be empty"
-	} else if utf8.RuneCountInString(title) > 100 {
-		fieldErrors["title"] = "field: title, may not be greater than 100 characters in length"
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "field: title, may not be empty"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "field: title, may not be greater than 100 characters in length"
 	}
 
-	if strings.TrimSpace(content) == "" {
-		fieldErrors["content"] = "field: content, may not be empty"
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "field: content, may not be empty"
 	}
 
-	if expires != 1 && expires != 7 && expires != 365 {
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
 		fieldErrors["expires"] = "field: expires, must have a selected value"
 	}
 
-	if len(fieldErrors) > 0 {
-		fmt.Fprint(w, fieldErrors)
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
