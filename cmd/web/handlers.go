2fd81eda1ac8e1cd3ab21025c51e033b779d1e8a
Badr <choubai.badr@protonmail.com>
2023-06-05 20:26:53 -0600

    Add missing return, causing balnk form to be submitted

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -84,6 +84,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
+		return
 	}
 
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
