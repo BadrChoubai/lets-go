3e08204c73868071c22952d684de6eaf6f555dcc
Badr <choubai.badr@protonmail.com>
2023-06-07 19:04:34 -0600

    Add user forms validator

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
@@ -8,6 +9,11 @@ import (
 type Validator struct {
 	FieldErrors map[string]string
 }
+
+var (
+	EmailRX = regexp.MustCompile(
+		"^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
 
 func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
@@ -29,12 +35,20 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 	}
 }
 
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
 func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
+}
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
 }
 
 func PermittedInt(value int, permittedValues ...int) bool {
