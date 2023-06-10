9f58f3e7d9a6be248b7d5cffb84c2b8a5660b7ef
Badr <choubai.badr@protonmail.com>
2023-06-09 19:40:50 -0600

    Add multiple tests

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,10 +6,36 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	tm := time.Date(1999, 3, 2, 12, 0, 0, 0, time.UTC)
-	hd := humanDate(tm)
-
-	if hd != "02 Mar 1999 UTC 12:00" {
-		t.Errorf("got %q; want %q", hd, "02 Mar 1999 UTC 12:00")
+	testTable := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.UTC),
+			want: "17 Mar 2022 UTC 10:15",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar 2022 UTC 09:15",
+		},
 	}
+
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+
+			if hd != tt.want {
+				t.Errorf("got %q; want %q;", hd, tt.want)
+			}
+		})
+	}
+
 }
