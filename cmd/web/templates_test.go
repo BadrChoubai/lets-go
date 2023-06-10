bc60199e95042a9bb54d421230e768cd54e8b58b
Badr <choubai.badr@protonmail.com>
2023-06-09 19:47:34 -0600

    Create assertion helper

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -1,40 +1,40 @@
 package main
 
 import (
+	"snippetbox.badrchoubai.dev/internal/assert"
 	"testing"
 	"time"
 )
 
 func TestHumanDate(t *testing.T) {
 	testTable := []struct {
-		name string
-		tm   time.Time
-		want string
+		name     string
+		tm       time.Time
+		expected string
 	}{
 		{
-			name: "UTC",
-			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.UTC),
-			want: "17 Mar 2022 UTC 10:15",
+			name:     "UTC",
+			tm:       time.Date(2022, 3, 17, 10, 15, 0, 0, time.UTC),
+			expected: "17 Mar 2022 UTC 10:15",
 		},
 		{
-			name: "Empty",
-			tm:   time.Time{},
-			want: "",
+			name:     "Empty",
+			tm:       time.Time{},
+			expected: "",
 		},
 		{
-			name: "CET",
-			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
-			want: "17 Mar 2022 UTC 09:15",
+			name:     "CET",
+			tm:       time.Date(2022, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			expected: "17 Mar 2022 UTC 09:15",
 		},
 	}
 
 	for _, tt := range testTable {
 		t.Run(tt.name, func(t *testing.T) {
-			hd := humanDate(tt.tm)
+			actual := humanDate(tt.tm)
 
-			if hd != tt.want {
-				t.Errorf("got %q; want %q;", hd, tt.want)
-			}
+			assert.Equal(t, actual, tt.expected)
+
 		})
 	}
 
